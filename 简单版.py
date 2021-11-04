61b234a29d4313472b0c25a7f77054709e73392c
James-boy <79518643+James-boy@users.noreply.github.com>
2021-11-04 19:05:53 +0800

    Add files via upload

---
+++ 简单版.py
@@ -0,0 +1,156 @@
+# 五子棋实现思路：
+# 1.设置一个列表用于记录棋盘上所有位置信息（是否有子和黑白子）
+# 2.监控鼠标状态，将鼠标落点位置读取，选择关闭界面或者落子。落子的同时将列表上
+# 该位置的信息进行改变。并同时判断是否输赢
+# 3.主程序，设置画布，然后读取列表，和导入图片（实现落子和背景、结束时的图像不一）。
+# 每次更新页面，将列表上数据信息显示为白子或黑子的导入相关图片，同时根据是否结束来改变页面
+
+import pygame
+import sys
+import time
+from pygame.locals import *
+
+# 用于记录棋盘信息，每个位置落子情况
+initChesslist = []
+
+# 1 表示是白子，2 表示是黑子
+initRole = 1   # 此处 1 表示第一个落白子
+
+# 用于判断是否结束
+resultFlag = 0
+
+# 用一个类来描述某棋盘格点的信息，x表示该格点横坐标，y表示该格点纵坐标，
+# 0 表示未落子，1 表示落了白子，2 表示落了黑子
+class StornPoint():
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+# 该函数用于得到棋盘初始信息
+def initChessSquare(x, y):
+    for i in range(15):
+        rowlist = []
+        for j in range(15):
+            pointx = x + j*41   # 上面的15和这里的41也是经过计算的，同样的根据棋盘格点数和每个格子所占像素大小确定
+            pointy = y + i*41
+            sp = StornPoint(pointx, pointy, 0)  # 各格点都属于这个类但是互不影响
+            rowlist.append(sp)
+        initChesslist.append(rowlist)
+
+# 此函数用于判断鼠标点击位置，若该位置某范围内存在格点，将格点信息进行更改，便于后续画图
+# 同时该函数调用了后一个函数，用于判断是否有五子相连，如果有，则返回某句话，同时返回一个resultFlag值
+def eventHander():
+    for event in pygame.event.get():
+        global initRole
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            i = 0
+            j = 0
+            for temp in initChesslist:
+                for point in temp:
+                    # 此处用于判断鼠标位置在哪个格点附近
+                    if x >= (point.x - 10) and x <= (point.x + 10) and \
+                            y >= (point.y - 10) and y <= (point.y + 10):
+                        if point.value == 0 and initRole == 1:
+                            point.value = 1
+                            judgeResult(i, j, 1)
+                            initRole = 2
+                        if point.value == 0 and initRole == 2:
+                            point.value = 2
+                            judgeResult(i, j, 2)
+                            initRole = 1
+                        break
+                    j += 1
+                i += 1
+                j = 0
+
+# 用于判断是否有五子相连，如果有，则返回某句话，同时返回一个resultFlag值
+def judgeResult(i, j, value):
+    global resultFlag
+    flag = False
+    # 横向赢
+    for x in range(i - 4, i + 5):
+        if x >= 0 and x + 4 < 15:
+            if initChesslist[x][j].value == value and \
+                initChesslist[x + 1][j].value == value and \
+                initChesslist[x + 2][j].value == value and \
+                initChesslist[x + 3][j].value == value and \
+                initChesslist[x + 4][j].value == value:
+                flag = True
+                break
+    # 纵向赢
+    for y in range(j - 4, j + 5):
+        if y >= 0 and y + 4 < 15:
+            if initChesslist[i][y].value == value and \
+                initChesslist[i][y + 1].value == value and \
+                initChesslist[i][y + 2].value == value and \
+                initChesslist[i][y + 3].value == value and \
+                initChesslist[i][y + 4].value == value:
+                flag = True
+                break
+    # 左上上上！！！方向赢
+    for x, y in zip(range(i - 4, i + 5), range(j - 4, j + 5)):
+        if x >= 0 and x + 4 < 15 and y >= 0 and y + 4 < 15:
+            if initChesslist[x][y].value == value and \
+                initChesslist[x + 1][y + 1].value == value and \
+                initChesslist[x + 2][y + 2].value == value and \
+                initChesslist[x + 3][y + 3].value == value and \
+                initChesslist[x + 4][y + 4].value == value:
+                flag = True
+                break
+    # 左下下下！！！方向赢
+    for x, y in zip(range(i - 4, i + 5), range(j + 4, j - 5, -1)):
+        if x >= 0 and x + 4 <= 15 and y - 4 >= 0 and y < 15:
+            if initChesslist[x][y].value == value and \
+                initChesslist[x + 1][y - 1].value == value and \
+                initChesslist[x + 2][y - 2].value == value and \
+                initChesslist[x + 3][y - 3].value == value and \
+                initChesslist[x + 4][y - 4].value == value:
+                flag = True
+                break
+    if flag:
+        resultFlag = value
+        print('白棋赢' if value == 1 else '黑棋赢')
+
+def main():
+    global initChesslist, resultFlag, initRole
+    initChessSquare(26, 26)   # 这个是经过计算的，根据棋盘照片的大小来写的，
+                              # 26是棋盘边缘到第一个格点的像素值大小，(15-1)*41是第一个格点到最后一个格点之间的距离
+                              # 总棋盘照片大小为26*2+(15-1)*41=626
+    pygame.init()
+    screen = pygame.display.set_mode((626, 626))  # 这个根据棋盘图像大小确定
+    pygame.display.set_caption('小二的五子棋')
+    background = pygame.image.load('image/棋盘.jpg')
+    resultStorn = pygame.image.load('image/风景图.jpg')
+    whiteStorn = pygame.image.load('image/白子.jpg')
+    blackStorn = pygame.image.load('image/黑子.jpg')
+    #whiteStorn.get_rect()
+    #blackStorn.get_rect()
+
+    while True:
+        screen.blit(background, (0, 0))
+        for temp in initChesslist:
+            for point in temp:
+                if point.value == 1:
+                    screen.blit(whiteStorn, (point.x - 18, point.y - 18))
+                elif point.value == 2:
+                    screen.blit(blackStorn, (point.x - 18, point.y - 18))
+
+        if resultFlag > 0:
+            initChesslist = []
+            initChessSquare(26, 26)
+            screen.blit(resultStorn, (0, 0))
+        pygame.display.update()
+
+        if resultFlag > 0:
+            time.sleep(3)
+            resultFlag = 0
+            initRole = 1  # 保证每次都是白棋先，如若不用，可再更改
+        eventHander()
+
+if __name__ == '__main__':
+    main()
---
+++ 进阶版2.py
@@ -0,0 +1,200 @@
+import pygame
+import sys
+import time
+from pygame.locals import *
+
+class StornPoint():
+    def __init__(self, x, y, value, number):
+        self.x = x
+        self.y = y
+        self.value = value
+        self.number = number
+
+firstRole = 1
+initChesslist = []
+initRole = firstRole
+resultFlag = 0
+k = 0
+
+class Five_Chess():
+    def initChessSquare(self, x, y):
+        for i in range(15):
+            rowlist = []
+            for j in range(15):
+                pointx = x + j * 41
+                pointy = y + i * 41
+                sp = StornPoint(pointx, pointy, 0, 0)  # 各格点都属于这个类但是互不影响
+                rowlist.append(sp)
+            initChesslist.append(rowlist)
+
+    def eventHander(self):
+        global initChesslist, initRole, resultFlag, k
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                i = 0
+                j = 0
+                for temp in initChesslist:
+                    for point in temp:
+                        # 此处用于判断鼠标位置在哪个格点附近
+                        if x >= point.x - 10 and x <= point.x + 10 and y >= point.y - 10 and y <= point.y + 10:
+                            if point.value == 0 and initRole == 1:
+                                point.value = 1
+                                point.number = k + 1
+                                k += 1
+                                self.judgeResult(i, j, 1)
+                                initRole = 2
+                            if point.value == 0 and initRole == 2:
+                                point.value = 2
+                                point.number = k + 1
+                                k += 1
+                                self.judgeResult(i, j, 2)
+                                initRole = 1
+                        j += 1
+                    i += 1
+                    j = 0
+                # 鼠标点击悔棋时进行如下代码，遍历列表，查看前一个落子的信息，并将其信息进行更改
+                if x >= 656 and x <= 716 and y >= 300 and y <= 330:
+                    for temp in initChesslist:
+                        for point in temp:
+                            if point.number == k:
+                                break  # 一检测到就跳出当前循环，不然会检索下一行，可能出现一下消失几个棋子
+                        if point.number == k and point.value == 1:
+                            point.value = 0
+                            point.number = 0
+                            k = k - 1
+                            initRole = 1
+                            break
+                        if point.number == k and point.value == 2:
+                            point.value = 0
+                            point.number = 0
+                            k = k - 1
+                            initRole = 2
+                            break
+
+                # 点击重新开始时执行如下代码
+                if x >= 656 and x <= 776 and y >= 400 and y <= 430:
+                    initChesslist = []
+                    self.initChessSquare(26, 26)
+                    k = 0
+                    initRole = firstRole  # 用于保证重新开始后白子先，若要每次黑子先则改为2就行
+                # 点击退出时执行如下代码
+                if x >= 656 and x <= 716 and y >= 500 and y <= 530:
+                    pygame.quit()
+                    sys.exit()
+
+    def judgeResult(slef, i, j, value):
+        global initChesslist, resultFlag
+        flag = False
+        # 横向赢
+        for x in range(i - 4, i + 5):
+            if x >= 0 and x + 4 < 15:
+                if initChesslist[x][j].value == value and \
+                        initChesslist[x + 1][j].value == value and \
+                        initChesslist[x + 2][j].value == value and \
+                        initChesslist[x + 3][j].value == value and \
+                        initChesslist[x + 4][j].value == value:
+                    flag = True
+                    break
+        # 纵向赢
+        for y in range(j - 4, j + 5):
+            if y >= 0 and y + 4 < 15:
+                if initChesslist[i][y].value == value and \
+                        initChesslist[i][y + 1].value == value and \
+                        initChesslist[i][y + 2].value == value and \
+                        initChesslist[i][y + 3].value == value and \
+                        initChesslist[i][y + 4].value == value:
+                    flag = True
+                    break
+        # 左上上上！！！方向赢
+        for x, y in zip(range(i - 4, i + 5), range(j - 4, j + 5)):
+            if x >= 0 and x + 4 < 15 and y >= 0 and y + 4 < 15:
+                if initChesslist[x][y].value == value and \
+                        initChesslist[x + 1][y + 1].value == value and \
+                        initChesslist[x + 2][y + 2].value == value and \
+                        initChesslist[x + 3][y + 3].value == value and \
+                        initChesslist[x + 4][y + 4].value == value:
+                    flag = True
+                    break
+        # 左下下下！！！方向赢
+        for x, y in zip(range(i - 4, i + 5), range(j + 4, j - 5, -1)):
+            if x >= 0 and x + 4 <= 15 and y - 4 >= 0 and y < 15:
+                if initChesslist[x][y].value == value and \
+                        initChesslist[x + 1][y - 1].value == value and \
+                        initChesslist[x + 2][y - 2].value == value and \
+                        initChesslist[x + 3][y - 3].value == value and \
+                        initChesslist[x + 4][y - 4].value == value:
+                    flag = True
+                    break
+        if flag:
+            resultFlag = value
+            print('白棋赢' if value == 1 else '黑棋赢')
+
+    def show_text(self, screen, txt, place_):
+        s_font = pygame.font.Font('字体文件/正楷字体.TTF', 30)
+        s_text = s_font.render(txt, True, (0, 0, 0))
+        screen.blit(s_text, place_)
+
+    def main(self):
+        global initChesslist, resultFlag, initRole, k
+        self.initChessSquare(26, 26)
+        pygame.init()
+        pygame.mixer.music.load('music/望江南.wav')
+        pygame.mixer.music.set_volume(1)
+        pygame.mixer.music.play(loops=-1, start=0.0)
+        screen = pygame.display.set_mode((826, 626))
+        pygame.display.set_caption('小二的五子棋')
+        rightground = pygame.image.load('image/棋盘.jpg')
+        leftground = pygame.image.load('image/褐色图片.jpg')
+        whiteStorn = pygame.image.load('image/白子.jpg')
+        blackStorn = pygame.image.load('image/黑子.jpg')
+        # whiteStorn.get_rect()
+        # blackStorn.get_rect()
+
+        while True:
+            screen.blit(rightground, (0, 0))
+            screen.blit(leftground, (626, 0))
+            self.show_text(screen, '悔棋', (656, 300))
+            self.show_text(screen, '重新开始', (656, 400))
+            self.show_text(screen, '退出', (656, 500))
+            for temp in initChesslist:
+                for point in temp:
+                    if point.value == 1:
+                        screen.blit(whiteStorn, (point.x - 18, point.y - 18))
+                    elif point.value == 2:
+                        screen.blit(blackStorn, (point.x - 18, point.y - 18))
+            # 用于在右上角展示下一步应该是哪个子
+            for temp in initChesslist:
+                for point in temp:
+                    if point.number == k:
+                        break
+                if point.value == 1:
+                    screen.blit(blackStorn, (708, 100))
+                    break
+                elif point.value == 2:
+                    screen.blit(whiteStorn, (708, 100))
+                    break
+            # 如若赢棋执行以下代码
+            if resultFlag > 0:
+                initChesslist = []
+                self.initChessSquare(26, 26)
+                k = 0
+                if resultFlag == 1:
+                    self.show_text(screen, '白棋赢', (20, 20))
+                    self.show_text(screen, '三秒后将重新开始...', (20, 60))
+                elif resultFlag == 2:
+                    self.show_text(screen, '黑棋赢', (20, 20))
+                    self.show_text(screen, '三秒后将重新开始...', (20, 60))
+                pygame.display.update()
+                time.sleep(3)
+                resultFlag = 0
+                initRole = firstRole
+            pygame.display.update()
+
+            self.eventHander()
+
+if __name__ == '__main__':
+    Five_Chess().main()
